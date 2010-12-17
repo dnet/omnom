2565441f7a4cb44dc7c1580fce7d7601b8e278d0
stef <stefan.marsiske@gmail.com>
2010-12-17 20:20:00 +0100

    [enh] added basic db model

--- tags/models.py
+++ tags/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=200) # yes longer tags than tweets! ;)
+
+class URI(models.Model):
+    tag = models.ManyToManyField(Tag)
+    url = models.TextField()
+    created = models.DateTimeField()
+    private = models.BooleanField()
+    title = models.TextField()
+    notes = models.TextField()
+
