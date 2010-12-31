35b98db5fc3b28eba460b8ba15d836c7aae9f5b0
stef <stefan.marsiske@gmail.com>
2010-12-31 22:07:05 +0100

    [enh] added basic conf

---
+++ conf.py
@@ -0,0 +1,11 @@
+delicioususer=''
+deliciouspassword=''
+peers=[('http://localhost:8002/','',''),]
+flickr={ 'key': '',
+         'secret': ''}
+openCalaisAPIKey=''
+
+try:
+    from conf-local import *
+except:
+    pass
